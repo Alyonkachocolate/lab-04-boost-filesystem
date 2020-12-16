f06fcb3dbbb79b847f78c7415070be0b12518015
Alyonkachocolate <Nyanyoka@yandex.ru>
2020-12-16 21:01:23 +0400

    Create tests and update function data()

--- include/header.hpp
+++ include/header.hpp
@@ -3,4 +3,44 @@
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
 
-#endif // INCLUDE_HEADER_HPP_
+#include <iostream>
+#include <map>
+
+bool financial_info(const std::string& name) {
+  if (name.size() != 25 || name.substr(0, 8) != "balance_" ||
+      name.at(16) != '_')
+    return false;
+  // если номер счёт и даты не из цифр
+  for (size_t i = 8; i < name.size(); i++)
+    if (i != 16)
+      if (!((int)name.at(i) >= 48 && (int)name.at(i) <= 57)) return false;
+  // проверим формат даты
+  if (stoi(name.substr(21, 2)) > 12) return false;
+  if (stoi(name.substr(23, 2)) > 31) return false;
+  return true;
+}
+
+// колличество документов по счёту, дата последнего
+struct Fin_file {
+  size_t count = 0;
+  std::string lastdate;
+  bool operator==(const Fin_file& r) const {
+    return (count == r.count && lastdate == r.lastdate);
+  }
+};
+
+// добавление элемента в map - если такого счёта нет или если дата новее
+void data(std::map<std::string, Fin_file>& summ_brok, const std::string& name) {
+  std::string number = name.substr(8, 8);
+  int date = stoi(name.substr(17, 8));
+
+  if (summ_brok.find(number) == summ_brok.end()) {
+    summ_brok[number].lastdate = name.substr(17, 8);
+  } else {
+    int date_ = stoi(summ_brok[number].lastdate.substr(0, 8));
+    if (date_ < date) summ_brok[number].lastdate = name.substr(17, 8);
+  }
+  summ_brok[number].count++;
+}
+
+#endif  // INCLUDE_HEADER_HPP_
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,38 @@
-// Copyright 2020 Your Name <your_email>
+// Copyright [2020] <Alyona Dorodnyaya>
 
 #include <gtest/gtest.h>
 
-TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+#include <header.hpp>
+
+TEST(Boost_filesystem, financial_info) {
+  EXPECT_TRUE(financial_info("balance_00000001_20180102"));
+  // неверный формат (размер != 25)
+  EXPECT_FALSE(financial_info("information"));
+  EXPECT_FALSE(financial_info("balance_"));
+  EXPECT_FALSE(financial_info("00000001_20180102"));
+  EXPECT_FALSE(financial_info("balance_1_20180102"));
+  // неверный формат счёта
+  EXPECT_FALSE(financial_info("balance_000а0001_20180102"));
+  // неверный формат даты
+  EXPECT_FALSE(financial_info("balance_01056804_201u0102"));
+  // неверный формат месяца
+  EXPECT_FALSE(financial_info("balance_01056804_20181502"));
+  // неверный формат дня
+  EXPECT_FALSE(financial_info("balance_01056804_20180134"));
+}
+
+TEST(Boost_filesystem, data) {
+  std::map<std::string, Fin_file> summ_brok;
+  data(summ_brok, "balance_01056804_20201102");
+  EXPECT_EQ(summ_brok["01056804"], (Fin_file{1, "20201102"}));
+  // тот же счёт, дата раньше (20201101 < 20201102)
+  data(summ_brok, "balance_01056804_20201101");
+  EXPECT_EQ(summ_brok["01056804"], (Fin_file{2, "20201102"}));
+  // тот же счёт, дата позднее (20201111 > 20201102)
+  data(summ_brok, "balance_01056804_20201111");
+  EXPECT_EQ(summ_brok["01056804"], (Fin_file{3, "20201111"}));
+  // новый счёт
+  data(summ_brok, "balance_11111111_20200214");
+  EXPECT_EQ(summ_brok["01056804"], (Fin_file{3, "20201111"}));
+  EXPECT_EQ(summ_brok["11111111"], (Fin_file{1, "20200214"}));
 }
