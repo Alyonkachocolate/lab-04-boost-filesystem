7c54ecd0e71b690a00a177029358338aa15e3586
Alyonkachocolate <Nyanyoka@yandex.ru>
2020-12-16 21:13:28 +0400

    Add exception

--- demo/main.cpp
+++ demo/main.cpp
@@ -8,71 +8,77 @@
 namespace fs = boost::filesystem;
 
 int main(int argc, char* argv[]) {
-//     для тестирования
-//    fs::path path_to_ftp = (argc > 1) ? argv[1] : "../test_file";
+  try {
+    //     для тестирования
+    //    fs::path path_to_ftp = (argc > 1) ? argv[1] : "../test_file";
 
-  fs::path path_to_ftp = (argc > 1) ? argv[1] : fs::current_path();
+    fs::path path_to_ftp = (argc > 1) ? argv[1] : fs::current_path();
 
-  if (!exists(path_to_ftp)) throw std::invalid_argument("Don't found FTP-file");
+    if (!exists(path_to_ftp))
+      throw std::invalid_argument("Don't found FTP-file");
 
-  // map: ключ - имя брокера, значение - счета, колличество документов по ним
-  // и дата последнего
-  std::map<std::string, std::map<std::string, Fin_file>> summ;
-  for (const fs::directory_entry& broker :
-       fs::directory_iterator{path_to_ftp}) {
-    // неверный формат файла - не состоит из дирикторий, ненайдена
-    // директория
-    if (!exists(broker.path()))
-      throw std::invalid_argument("Invalid FTP-file: don't found\"" +
-                                  broker.path().string() + "\"");
-    if (!is_directory(broker.path()))
-      throw std::invalid_argument("Invalid FTP-file: \"" +
-                                  broker.path().string() +
-                                  "\" isn't directory");
-
-    // проходимся по дириктории
-    // map: ключ - счёт, значение - колличество документов по ним
+    // map: ключ - имя брокера, значение - счета, колличество документов по ним
     // и дата последнего
-    std::map<std::string, Fin_file> summ_brok;
-    for (const fs::directory_entry& x : fs::directory_iterator{broker.path()}) {
-      auto file = x.path();
-
-      // неверный формат файла - ненайден файл, это директория
-      if (!exists(file))
+    std::map<std::string, std::map<std::string, Fin_file>> summ;
+    for (const fs::directory_entry& broker :
+         fs::directory_iterator{path_to_ftp}) {
+      // неверный формат файла - не состоит из дирикторий, ненайдена
+      // директория
+      if (!exists(broker.path()))
         throw std::invalid_argument("Invalid FTP-file: don't found\"" +
-                                    file.string() + "\"");
+                                    broker.path().string() + "\"");
+      if (!is_directory(broker.path()))
+        throw std::invalid_argument("Invalid FTP-file: \"" +
+                                    broker.path().string() +
+                                    "\" isn't directory");
 
-      if (is_directory(file))
-        throw std::invalid_argument("Invalid FTP-file: \"" + file.string() +
-                                    "\" isn't file");
+      // проходимся по дириктории
+      // map: ключ - счёт, значение - колличество документов по ним
+      // и дата последнего
+      std::map<std::string, Fin_file> summ_brok;
+      for (const fs::directory_entry& x :
+           fs::directory_iterator{broker.path()}) {
+        auto file = x.path();
 
-      // если символьная ссылка - проверяем еще раз на существование файла
-      if (is_symlink(file)) {
-        file = read_symlink(file);
+        // неверный формат файла - ненайден файл, это директория
         if (!exists(file))
           throw std::invalid_argument("Invalid FTP-file: don't found\"" +
                                       file.string() + "\"");
-      }
 
-      // если это файл, с разрешением .txt, удовлетворяющий условию
-      if (is_regular_file(file) && file.extension().string() == ".txt" &&
-          file.stem().extension().string() != ".old" &&
-          financial_info(file.stem().string())) {
-        // для первого задания
-        std::cout << broker.path().stem().string() << ' '
-                  << file.filename().string() << std::endl;
+        if (is_directory(file))
+          throw std::invalid_argument("Invalid FTP-file: \"" + file.string() +
+                                      "\" isn't file");
 
-        data(summ_brok, file.stem().string());
+        // если символьная ссылка - проверяем еще раз на существование файла
+        if (is_symlink(file)) {
+          file = read_symlink(file);
+          if (!exists(file))
+            throw std::invalid_argument("Invalid FTP-file: don't found\"" +
+                                        file.string() + "\"");
+        }
+
+        // если это файл, с разрешением .txt, удовлетворяющий условию
+        if (is_regular_file(file) && file.extension().string() == ".txt" &&
+            file.stem().extension().string() != ".old" &&
+            financial_info(file.stem().string())) {
+          // для первого задания
+          std::cout << broker.path().stem().string() << ' '
+                    << file.filename().string() << std::endl;
+
+          data(summ_brok, file.stem().string());
+        }
       }
+      summ[broker.path().stem().string()] = summ_brok;
     }
-    summ[broker.path().stem().string()] = summ_brok;
+    std::cout << '\n';
+
+    for (const auto& broker : summ)
+      for (const auto& number : broker.second) {
+        std::cout << "broker:" << broker.first << " account:" << number.first;
+        std::cout << " files: " << number.second.count
+                  << " lastdate:" << number.second.lastdate << std::endl;
+      }
+  } catch (const std::exception& er) {
+    std::cout << "Error. " << er.what() << std::endl;
   }
-  std::cout << '\n';
-
-  for (const auto& broker : summ)
-    for (const auto& number : broker.second) {
-      std::cout << "broker:" << broker.first << " account:" << number.first;
-      std::cout << " files: " << number.second.count
-                << " lastdate:" << number.second.lastdate << std::endl;
-    }
 }
